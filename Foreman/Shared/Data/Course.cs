2e2ea9c364567f47f8d01e924f78d8df097014cf
Jakub Walkowski <walkoskuba@gmail.com>
2021-12-19 16:08:12 +0100

    net6

---
+++ Foreman/Shared/Data/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreman.Shared.Data
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public int? CourseCategoryId { get; set; }
+        public int? InstitutionId { get; set; }
+        public string ShortName { get; set; }
+        public string FullName { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+
+        public CourseCategory Category { get; set; }
+    }
+}
---
+++ Foreman/Shared/Data/CourseCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreman.Shared.Data
+{
+    public class CourseCategory
+    {
+        public int Id { get; set; }
+        public int? ParentCategory { get; set; }
+        public int? InstitutionId { get; set; }
+        public bool IsVisible { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
