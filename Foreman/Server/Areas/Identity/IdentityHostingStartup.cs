ab04f6f9b01cbf608642ee0471854a2f96b5d7a2
mateuszp <piskorzm@hotmail.com>
2022-01-09 18:17:58 +0100

    Identity scaffold

---
+++ Foreman/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Foreman.Server.Data;
+using Foreman.Shared.Data.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Foreman.Server.Areas.Identity.IdentityHostingStartup))]
+namespace Foreman.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
