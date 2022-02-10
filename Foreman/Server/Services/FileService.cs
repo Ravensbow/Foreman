e061bb5d1fe9a33be58f9a60c597cfe0ffef3b22
mateuszp <piskorzm@hotmail.com>
2022-02-10 20:48:16 +0100

    FileService - WIP

---
+++ Foreman/Server/Services/FileService.cs
@@ -0,0 +1,88 @@
+﻿using Foreman.Server.Data;
+using Microsoft.AspNetCore.Http;
+using System.Security.Cryptography;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System;
+using System.IO;
+using System.Linq;
+using Foreman.Server.Utility;
+
+namespace Foreman.Server.Services
+{
+    public class FileService : IFileService
+    {
+        private ApplicationContext _context;
+        private IConfiguration _configuration;
+        public string StorageDirectory { get; }
+        public string StoragePath { get; }
+        public FileService(ApplicationContext db, IConfiguration config)
+        {
+            _context = db;
+            _configuration = config;
+            StorageDirectory = _configuration.GetSection("ForemanStorage")
+                .GetValue<string>("DirectoryName");
+            StoragePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, StorageDirectory);
+        }
+
+        public Result DeleteFile(int dbFileId)
+        {
+            try
+            {
+                var model = _context.Files.Where(f => f.Id == dbFileId).Single();
+                _context.Files.Remove(model);
+
+                if(!_context.Files.Where(f => f.ContentHash == model.ContentHash).Any() && File.Exists(Path.Combine(StoragePath,model.ContentHash)))
+                {
+                    File.Delete(Path.Combine(StoragePath, model.ContentHash));
+                }
+
+                return Result.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Result.Fail(ex.Message);
+            }
+
+        }
+
+        public Result StoreFile(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<byte[]> GetFile(string fileHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckIfStorageDirectoryExists()
+        {
+            return Directory.Exists(StoragePath);
+        }
+
+        public void EnsureStorageDirectoryExists()
+        {
+            Directory.CreateDirectory(StoragePath);
+        }
+
+        private string HashToString(byte[] hashByteArr)
+        {
+            return BitConverter.ToString(hashByteArr).Replace("-",string.Empty);
+        }
+
+        private byte[] HashFunction(string source)
+        {
+            var hash = SHA1.Create();
+            byte[] sourceByteArr = Encoding.UTF8.GetBytes(source);
+            return hash.ComputeHash(sourceByteArr);
+
+        }
+
+        private byte[] HasFunction(byte[] source)
+        {
+            var hash = SHA1.Create();
+            return hash.ComputeHash(source);
+        }
+    }
+}
